8e30ffe335a4f7a783fafabe7b1f8952d81d868d
papazianz <37383732+papazianz@users.noreply.github.com>
2018-10-11 12:49:27 -0400

    Update Rock.java

--- Rock.java
+++ Rock.java
@@ -1,94 +1,94 @@
 import java.awt.Polygon;
 
-public class Rock extends Polygon()
+public class Rock extends Polygon
 {
-    // Upper left corner of actual polygon object
-    int upLeftXPos, upLeftYPos;
+   // Upper left corner postion of actual rock object
+   int rockUpLeftXPos, rockUpLeftYPos;
 
-    // Direction in which the asteriod(polygon) is moving
-    int xDirection = 1;
-    int yDirection = 1;
+   // Direction in which the asteriod(polygon) is moving
+   int xDirection = 1;
+   int yDirection = 1;
 
-    // Gameboard width and height
-    int width = ex.boardwidth;
-    int height = ex.boardheigt;
+   // Gameboard width and height
+   int width = GameBoard.boardWidth;
+   int height = GameBoard.boardHeight;
 
-    // x and y cordinates for the location of each asteriod(polygon)
-    int[] polyXArray, polyYArray;
+   // x and y cordinates for the location of each asteriod(polygon)
+   int[] rockXArray, rockYArray;
 
-    // starting x and y ccordinates for the asteriods(polygon)
-    public static int[] sPolyXArray = {10,17,26,34,27,36,26,14,8,1,5,1,10};
-    public static int[] sPolyYArray = {0,5,1,8,13,20,31,28,31,22,16,7,0};
+   // starting x and y ccordinates for the asteriods(polygon)
+   public static int[] sRockXArray = {10,17,26,34,27,36,26,14,8,1,5,1,10};
+   public static int[] sRockYArray = {0,5,1,8,13,20,31,28,31,22,16,7,0};
 
-    // Creats astroid(polgon object)
-    public Rock(int[] polyXArray, int[] polyYArray, int pointInPoly, int randomStartXPos, int randomStartYPos )
-    {
-        super(polyXArray, polyYArray, pointInPoly)
-        
+   // Constructor for the rock 
+   public Rock(int[] rockXArray, int[] rockYArray, int pointInPoly, int randomStartXPos, int randomStartYPos )
+   {
+      super(rockXArray, rockYArray, pointInPoly);
+      
+   
+      // Moves the asteroid in a random direction
+      this.xDirection = (int) (Math.random() * 6 + 1);
+      this.yDirection = (int) (Math.random() * 6 + 1);
+   
+      // Sets starting point for astroid
+      this.rockUpLeftXPos = randomStartXPos;
+      this.rockUpLeftYPos = randomStartYPos;
+   
+   
+   }
 
-        // Moves the astroid in a random direction
-        this.xDirection = (int) (Math.random() * 4 + 1);
-        this.yDirection = (int) (Math.random() * 4 + 1);
-
-        // Sets starting point for astroid
-        this.upLeftXPos = randomStartXPos;
-        this.upLeftYPos = randomStartYPos;
-
-
-    }
-
-    public void move() 
-    {
-        // Places the cordinates of each rock in the array of polygon points
-        int upLeftXPos = super.xpoints[];
-        int upLeftYPos = super.ypoints[];
-
-        // reverses the direction of astroid as it hits the edge 
-        if (upLeftXPos > 0 || (upLeftXPos + 25 > width)
-            xDirection = - xDirection;
-        if (upLeftYPos > 0 || (upLeftYPos + 50 > height)
-            yDirection = - yDirection;
-
-        // Constenly moves the astroids by adding 
-        // the random direction to the the x and y cordinates
-        for (int i = 0; i < super.xpoints.length; i++)
-        {
-            super.xpoints[i] += xDirection;
-            super.ypoints[i] += yDirection;
-        }
-                
-        
-    }
-
-
-    // Gets a list of all the x postions of each asteroid by 
-    // cloning the orginal list, and adding the asteroids starting postion
-    public static int[] getPolyXArray(int randomStartXPos)
-    {
-        int[] tempPolyXArray = (int[])sPolyXArray.clone();
-
-        for (int i = 0; i < tempPolyXArray.length; i++)
-        {
-            tempPolyXArray += randomStartXPos;
-        }
-
-        return tempPolyXArray
-        
-    }
+   public void move() 
+   {
+      // Places the cordinates of each rock in the array of polygon cordinates
+      int rockUpLeftXPos = super.xpoints[0];
+      int rockUpLeftYPos = super.ypoints[0];
+   
+      // reverses the direction of astroid as it hits the edge 
+      if (rockUpLeftXPos < 0 || (rockUpLeftXPos + 25 > width))
+         xDirection = - xDirection;
+      if (rockUpLeftYPos < 0 || (rockUpLeftYPos + 50 > height))
+         yDirection = - yDirection;
+   
+      // Constenly moves the astroids by adding 
+      // the random direction to the the x and y cordinates
+      for (int i = 0; i < super.xpoints.length; i++)
+      {
+         super.xpoints[i] += xDirection;
+         super.ypoints[i] += yDirection;
+      }
+              
+      
+   }
 
 
-    // Gets a list of all the y postions of each asteroid by 
-    // cloning the orginal list, and adding the asteroids starting postion
-    public static int[] getPolyYArray(int randomStartYPos)
-    {
-        int[] tempPolyYArray = (int[])sPolyYArray.clone();
+   // Gets a list of all the x postions of each asteroid by 
+   // cloning the orginal list, and adding the asteroids starting postion
+   public static int[] getRockXArray(int randomStartXPos)
+   {
+      int[] tempRockXArray = (int[])sRockXArray.clone();
+   
+      for (int i = 0; i < tempRockXArray.length; i++)
+      {
+         tempRockXArray[i] += randomStartXPos;
+      }
+   
+      return tempRockXArray;
+      
+   }
 
-        for (int i = 0; i < tempPolyYArray.length; i++)
-        {
-            tempPolyYArray += randomStartYPos;
-        }
 
-        return tempPolyYArray
-        
-    }
+   // Gets a list of all the y postions of each asteroid by 
+   // cloning the orginal list, and adding the asteroids starting postion
+   public static int[] getRockYArray(int randomStartYPos)
+   {
+      int[] tempRockYArray = (int[])sRockYArray.clone();
+   
+      for (int i = 0; i < tempRockYArray.length; i++)
+      {
+         tempRockYArray[i] += randomStartYPos;
+      }
+   
+      return tempRockYArray;
+      
+   }
 }
