3f3831e0ac64a9f1ca9824cb37af570822b7140c
papazianz <37383732+papazianz@users.noreply.github.com>
2018-10-23 11:06:36 -0400

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -8,13 +8,23 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+
+
 
 
 public class GameBoard extends JFrame
 {
-   // Width and height og the game board
-   public static int boardHeight = 1000;
-   public static int boardWidth = 800;
+   // Width and height of the game board
+   public static int boardHeight = 800;
+   public static int boardWidth = 1200;
+
+   public static boolean keyHeld = false;
+   public static int keyHeldCode;
+   
+   public static int rotationAngle = 0;
 
    public static void main(String [] args)
    {
@@ -32,6 +42,52 @@ public class GameBoard extends JFrame
    
       // Fits the drawing to the whole screen
       this.add(gamePanel, BorderLayout.CENTER);
+      
+      // Allows the program to know when the player is using the keyboard to control the ship
+      addKeyListener(
+         new KeyListener() {
+         
+            @Override
+            public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
+            
+            }
+         
+         
+            @Override
+            public void keyPressed(KeyEvent e) {
+               if (e.getKeyCode()==87)
+               {
+                  System.out.println("Forward");
+               } 
+               else if (e.getKeyCode()==83)
+               {
+                  System.out.println("Backward");
+               }
+               else if (e.getKeyCode()==68)
+               {
+                  System.out.println("Rotate Right");
+               
+                  keyHeldCode = e.getKeyCode();
+                  keyHeld = true;
+               }
+               else if (e.getKeyCode()==65)
+               {
+                  System.out.println("Rotate left");
+               
+                  keyHeldCode = e.getKeyCode();
+                  keyHeld = true;
+               }
+            
+            
+            }
+         
+            @Override
+            public void keyReleased(KeyEvent e) {
+               keyHeld = false;
+            }
+            
+         });                
    
       ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
    
@@ -58,19 +114,20 @@ public class GameBoard extends JFrame
       {
          theBoard.repaint();
       }
-   
    }
 
    class DrawingPanel extends JComponent
    {
       // Holds all the rock objects
       public ArrayList<Rock> rocks = new ArrayList<Rock>();
-   
+      // Sets the x and y cordinates for each rock to there starting postions
       int[] rockXArray = Rock.sRockXArray;
       int[] rockYArray = Rock.sRockYArray;
    
       int width = GameBoard.boardWidth;
       int height = GameBoard.boardHeight;
+      
+      Ship theShip = new Ship();
    
       // Draws 50 rocks on game board and stores them in ArrayList<Rock>
       public DrawingPanel()
@@ -83,8 +140,6 @@ public class GameBoard extends JFrame
          
             rocks.add(new Rock(Rock.getRockXArray( randomStartXPos), 
                    Rock.getRockYArray(randomStartYPos), 13, randomStartXPos, randomStartYPos));
-         
-         
          }
       
       }
@@ -92,23 +147,43 @@ public class GameBoard extends JFrame
       public void paint(Graphics g) 
       {
          Graphics2D graphicSettings = (Graphics2D)g;
+         AffineTransform identity = new AffineTransform();
       
+      
+         //Sets backround color and dimen
          graphicSettings.setColor(Color.BLACK);
          graphicSettings.fillRect(0, 0, getWidth(), getHeight());
+         
+         
          graphicSettings.setRenderingHint( RenderingHints.KEY_ANTIALIASING, 
             RenderingHints.VALUE_ANTIALIAS_ON);
          graphicSettings.setPaint( Color.WHITE);
       
+         //Draws and moves each rock on gameboard
          for (Rock rock : rocks)
          {
             rock.move();
             graphicSettings.draw(rock);
          }
+         
+         
+         if(GameBoard.keyHeld == true && GameBoard.keyHeldCode == 68)
+         {
+            Ship.rotationAngle += 10;
+         }
+         else if(GameBoard.keyHeld == true && GameBoard.keyHeldCode == 65)
+         {
+            Ship.rotationAngle -= 10;
+         }
+         theShip.move();
+         
+         graphicSettings.setTransform(identity);
+         graphicSettings.translate(GameBoard.boardWidth/2,GameBoard.boardHeight/2);
+         graphicSettings.rotate(Math.toRadians(Ship.rotationAngle));
       
       
       
-      
-      
+         graphicSettings.draw(theShip);
       }
    }
 
